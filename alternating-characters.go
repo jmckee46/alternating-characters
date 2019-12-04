7216da78a4e5544ee2edd0ece0c0535240afc0a7
John McKee <jmckee3@mac.com>
2019-12-04 15:57:14 -0800

    working version

--- alternating-characters.go
+++ alternating-characters.go
@@ -1,3 +1,16 @@
 package main
 
+func alternatingCharacters(s string) int32 {
+	var deletions int32
+	sLength := int32(len(s))
+
+	for i := int32(1); i < sLength; i++ {
+		if s[i-1] == s[i] {
+			deletions++
+		}
+	}
+
+	return deletions
+}
+
 func main() {}
--- spec-alternating-characters_test.go
+++ spec-alternating-characters_test.go
@@ -2,6 +2,52 @@ package main
 
 import "testing"
 
-func TestAlternatingCharacters(t *testing.T) {
+func TestAlternatingCharacters1(t *testing.T) {
+	s := "AAAA"
 
+	deletions := alternatingCharacters(s)
+
+	if deletions != 3 {
+		t.Errorf("got %d instead of 3", deletions)
+	}
+}
+
+func TestAlternatingCharacters2(t *testing.T) {
+	s := "BBBBB"
+
+	deletions := alternatingCharacters(s)
+
+	if deletions != 4 {
+		t.Errorf("got %d instead of 4", deletions)
+	}
+}
+
+func TestAlternatingCharacters3(t *testing.T) {
+	s := "ABABABAB"
+
+	deletions := alternatingCharacters(s)
+
+	if deletions != 0 {
+		t.Errorf("got %d instead of 0", deletions)
+	}
+}
+
+func TestAlternatingCharacters4(t *testing.T) {
+	s := "BABABA"
+
+	deletions := alternatingCharacters(s)
+
+	if deletions != 0 {
+		t.Errorf("got %d instead of 0", deletions)
+	}
+}
+
+func TestAlternatingCharacters5(t *testing.T) {
+	s := "AAABBB"
+
+	deletions := alternatingCharacters(s)
+
+	if deletions != 4 {
+		t.Errorf("got %d instead of 4", deletions)
+	}
 }
